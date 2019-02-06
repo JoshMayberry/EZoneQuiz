92dacad1232152c23e17a686337c0e22544e8773
HOME\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2019-02-05 22:52:40 -0500

    Added images to the quiz. All images are free to use without citing them or from EmergencyZone.com

---
+++ app/src/main/java/com/example/android/ezonequiz/Question.java
@@ -0,0 +1,196 @@
+package com.example.android.ezonequiz;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+enum QuestionType {
+    Single, //Uses radio buttons
+    Multiple, //Uses check boxes
+    Integer, //Uses a number spinner
+    String, //Uses an edit text view
+}
+
+/**
+ * A container to store data about the question in.
+ */
+public class Question {
+    private QuizActivity activity;
+    private QuestionType type; //Which type of question this is
+    private int textId; //Which question to ask
+    private int correctId; //What text to show when the answer was correct
+    private int incorrectId; //What text to show when the answer was incorrect
+    private int rangeMin; //Used for Number Spinners
+    private int rangeMax; //Used for Number Spinners
+    private int correctValue; //Used for Number Spinners
+    private List<Integer> correctIndex = new ArrayList<>(); //The index numbers of the correct answers
+    private List<Integer> answerList = new ArrayList<>(); //Which answers belong to this question
+    private List<View> viewList = new ArrayList<>(); //Which widgets to use for the answers
+    private int imageId; //Which image to use for this question
+
+    /**
+     * Used for questions that use the radio buttons.
+     * @param textId - The resource id for the string to use for the question
+     * @param correctId - The resource id for the string to use for the correct answer message
+     * @param incorrectId - The resource id for the string to use for the incorrect answer message
+     * @param answerIdList - The resource ids for the strings to use for the answer widgets
+     * @param correctIndex - The index of the correct answer
+     */
+    Question(QuizActivity activity, int imageId, int textId, int correctId, int incorrectId, int[] answerIdList, int correctIndex){
+        this.activity = activity;
+        this.type = QuestionType.Single;
+        this.textId = textId;
+        this.imageId = imageId;
+        this.correctId = correctId;
+        this.incorrectId = incorrectId;
+
+        for (int i=0; i<answerIdList.length; i++) {
+            this.answerList.add(answerIdList[i]);
+            this.viewList.add(activity.view_radioButton[i]);
+        }
+
+        assert this.answerList.size() > correctIndex;
+        this.correctIndex.add(correctIndex);
+
+    }
+    /**
+     * Used for questions that use the check boxes.
+     * @param textId - The resource id for the string to use for the question
+     * @param correctId - The resource id for the string to use for the correct answer message
+     * @param incorrectId - The resource id for the string to use for the incorrect answer message
+     * @param answerIdList - The resource ids for the strings to use for the answer widgets
+     * @param correctIndex - The indexes of the correct answers
+     */
+    Question(QuizActivity activity, int imageId, int textId, int correctId, int incorrectId, int[] answerIdList, int[] correctIndex){
+        this.activity = activity;
+        this.type = QuestionType.Multiple;
+        this.textId = textId;
+        this.imageId = imageId;
+        this.correctId = correctId;
+        this.incorrectId = incorrectId;
+
+        for (int i=0; i<answerIdList.length; i++) {
+            this.answerList.add(answerIdList[i]);
+            this.viewList.add(this.activity.view_checkBox[i]);
+        }
+
+        for (int i: correctIndex) {
+            assert this.answerList.size() > i;
+            this.correctIndex.add(i);
+        }
+    }
+
+    /**
+     * Used for questions with a number spinner.
+     * @param textId - The resource id for the string to use for the question
+     * @param correctId - The resource id for the string to use for the correct answer message
+     * @param incorrectId - The resource id for the string to use for the incorrect answer message
+     * @param rangeMin - The lowest value the number spinner can have
+     * @param rangeMax - The highest value the number spinner can have
+     * @param correctValue - The correct value to select
+     */
+    Question(QuizActivity activity, int imageId, int textId, int correctId, int incorrectId, int rangeMin, int rangeMax, int correctValue) {
+        this.activity = activity;
+        this.type = QuestionType.Integer;
+        this.textId = textId;
+        this.imageId = imageId;
+        this.correctId = correctId;
+        this.incorrectId = incorrectId;
+        this.rangeMin = rangeMin;
+        this.rangeMax = rangeMax;
+
+        assert rangeMin <= correctValue;
+        assert rangeMax >= correctValue;
+        this.correctValue = correctValue;
+        this.viewList.add(this.activity.view_numberPicker);
+    }
+
+    /**
+     * Hides this question on the screen.
+     */
+    public void hide() {
+        for (View view: this.activity.currentQuestion.viewList) {
+            view.setVisibility(View.GONE);
+        }
+    }
+    /**
+     * Shows this question on the screen.
+     */
+    public void show() {
+        for (View view : this.activity.currentQuestion.viewList) {
+            view.setVisibility(View.VISIBLE);
+        }
+
+        this.activity.view_image.setImageDrawable(ContextCompat.getDrawable(activity, this.imageId));
+        this.activity.view_questionText.setText(this.activity.currentQuestion.textId);
+
+        switch (this.type) {
+            case Single:
+                for (int i = 0; i < this.viewList.size(); i++) {
+                    RadioButton view = (RadioButton) viewList.get(i);
+                    view.setText(this.answerList.get(i));
+                }
+                this.activity.view_radioGroup.clearCheck();
+                break;
+            case Multiple:
+                for (int i = 0; i < this.viewList.size(); i++) {
+                    CheckBox view = (CheckBox) viewList.get(i);
+                    view.setText(this.answerList.get(i));
+                    view.setChecked(false);
+                }
+                break;
+            case Integer:
+                //See: https://stackoverflow.com/questions/17805040/how-to-create-a-number-picker-dialog/17806895#17806895
+                this.activity.view_numberPicker.setMinValue(this.rangeMin);
+                this.activity.view_numberPicker.setMaxValue(this.rangeMax);
+                //Use: https://stackoverflow.com/questions/12979643/change-the-step-size-of-a-numberpicker/30469826#30469826
+        }
+    }
+    /**
+     * Checks the user's answer.
+     * @return - Whether or not the user chose correctly
+     */
+    public boolean check() {
+        switch (this.type) {
+            case Single:
+                for (int i = 0; i < this.viewList.size(); i++) {
+                    RadioButton view = (RadioButton) viewList.get(i);
+                    if (view.isChecked() != this.correctIndex.contains(i)) {
+                        return false;
+                    }
+                }
+                return true;
+            case Multiple:
+                for (int i = 0; i < this.viewList.size(); i++) {
+                    CheckBox view = (CheckBox) viewList.get(i);
+                    if (view.isChecked() != this.correctIndex.contains(i)) {
+                        return false;
+                    }
+                }
+                return true;
+            case Integer:
+                return this.activity.view_numberPicker.getValue() == this.correctValue;
+        }
+        return false;
+    }
+    /**
+     * Returns a formatted message saying that the user chose correctly
+     * @return The message to show in the dialog box
+     * See: https://www.baeldung.com/java-random-list-element#highlighter_812240
+     */
+    public String getMessage_correct() {
+        return this.activity.getResources().getString(this.correctId, this.activity.correctList[this.activity.random.nextInt(this.activity.correctList.length)]);
+    }
+    /**
+     * Returns a formatted message saying that the user chose incorrectly
+     * @return The message to show in the dialog box
+     */
+    public String getMessage_incorrect() {
+        return this.activity.getResources().getString(this.incorrectId, this.activity.incorrectList[this.activity.random.nextInt(this.activity.incorrectList.length)]);
+    }
+}
--- app/src/main/java/com/example/android/ezonequiz/QuizActivity.java
+++ app/src/main/java/com/example/android/ezonequiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -18,21 +19,23 @@ import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private RadioButton[] view_radioButton = new RadioButton[5];
-    private CheckBox[] view_checkBox = new CheckBox[5];
-    private RadioGroup view_radioGroup;
-    private NumberPicker view_numberPicker;
-    private TextView view_questionText;
+    public RadioButton[] view_radioButton = new RadioButton[5];
+    public CheckBox[] view_checkBox = new CheckBox[5];
+    public NumberPicker view_numberPicker;
+    public RadioGroup view_radioGroup;
+    public TextView view_questionText;
+    public Spinner view_spinner;
+    public ImageView view_image;
+
     private ProgressBar view_progress;
-    private Spinner view_spinner;
 
     private List<Question> questions = new ArrayList<>();
-    private Question currentQuestion;
+    public Question currentQuestion;
 
     private int score;
-    private Random random = new Random();
-    private String[] correctList = new String[3]; //Different ways to say correct
-    private String[] incorrectList = new String[2]; //Different ways to say incorrect
+    public Random random = new Random();
+    public String[] correctList = new String[3]; //Different ways to say correct
+    public String[] incorrectList = new String[2]; //Different ways to say incorrect
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +58,7 @@ public class QuizActivity extends AppCompatActivity {
         view_spinner = findViewById(R.id.spinner);
         view_questionText = findViewById(R.id.quiz_title);
         view_radioGroup = findViewById(R.id.radioGroup);
+        view_image = findViewById(R.id.quiz_image);
 
         correctList[0] = getResources().getString(R.string.correct_1);
         correctList[1] = getResources().getString(R.string.correct_2);
@@ -72,216 +76,46 @@ public class QuizActivity extends AppCompatActivity {
      * See: https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
      * See: https://stackoverflow.com/questions/38758220/android-enum-set-enum-programatically-from-integer-value/38758335#38758335
      */
-    public enum QuestionType {
-        Single, //Uses radio buttons
-        Multiple, //Uses check boxes
-        Integer, //Uses a number spinner
-        String, //Uses an edit text view
-    }
-
-    /**
-     * A container to store data about the question in.
-     */
-    private class Question {
-        QuestionType type; //Which type of question this is
-        int textId; //Which question to ask
-        int correctId; //What text to show when the answer was correct
-        int incorrectId; //What text to show when the answer was incorrect
-        int rangeMin; //Used for Number Spinners
-        int rangeMax; //Used for Number Spinners
-        int correctValue; //Used for Number Spinners
-        List<Integer> correctIndex = new ArrayList<>(); //The index numbers of the correct answers
-        List<Integer> answerList = new ArrayList<>(); //Which answers belong to this question
-        List<View> viewList = new ArrayList<>(); //Which widgets to use for the answers
-
-        /**
-         * Used for questions that use the radio buttons.
-         * @param textId - The resource id for the string to use for the question
-         * @param correctId - The resource id for the string to use for the correct answer message
-         * @param incorrectId - The resource id for the string to use for the incorrect answer message
-         * @param answerIdList - The resource ids for the strings to use for the answer widgets
-         * @param correctIndex - The index of the correct answer
-         */
-        Question(int textId, int correctId, int incorrectId, int[] answerIdList, int correctIndex){
-            this.type = QuestionType.Single;
-            this.textId = textId;
-            this.correctId = correctId;
-            this.incorrectId = incorrectId;
-
-            for (int i=0; i<answerIdList.length; i++) {
-                this.answerList.add(answerIdList[i]);
-                this.viewList.add(view_radioButton[i]);
-            }
-
-            assert this.answerList.size() > correctIndex;
-            this.correctIndex.add(correctIndex);
-
-        }
-        /**
-         * Used for questions that use the check boxes.
-         * @param textId - The resource id for the string to use for the question
-         * @param correctId - The resource id for the string to use for the correct answer message
-         * @param incorrectId - The resource id for the string to use for the incorrect answer message
-         * @param answerIdList - The resource ids for the strings to use for the answer widgets
-         * @param correctIndex - The indexes of the correct answers
-         */
-        Question(int textId, int correctId, int incorrectId, int[] answerIdList, int[] correctIndex){
-            this.type = QuestionType.Multiple;
-            this.textId = textId;
-            this.correctId = correctId;
-            this.incorrectId = incorrectId;
-
-            for (int i=0; i<answerIdList.length; i++) {
-                this.answerList.add(answerIdList[i]);
-                this.viewList.add(view_checkBox[i]);
-            }
-
-            for (int i: correctIndex) {
-                assert this.answerList.size() > i;
-                this.correctIndex.add(i);
-            }
-        }
-
-        /**
-         * Used for questions with a number spinner.
-         * @param textId - The resource id for the string to use for the question
-         * @param correctId - The resource id for the string to use for the correct answer message
-         * @param incorrectId - The resource id for the string to use for the incorrect answer message
-         * @param rangeMin - The lowest value the number spinner can have
-         * @param rangeMax - The highest value the number spinner can have
-         * @param correctValue - The correct value to select
-         */
-        Question(int textId, int correctId, int incorrectId, int rangeMin, int rangeMax, int correctValue) {
-            this.type = QuestionType.Integer;
-            this.textId = textId;
-            this.correctId = correctId;
-            this.incorrectId = incorrectId;
-            this.rangeMin = rangeMin;
-            this.rangeMax = rangeMax;
-
-            assert rangeMin <= correctValue;
-            assert rangeMax >= correctValue;
-            this.correctValue = correctValue;
-            this.viewList.add(view_numberPicker);
-        }
-
-        /**
-         * Hides this question on the screen.
-         */
-        public void hide() {
-            for (View view: currentQuestion.viewList) {
-                view.setVisibility(View.GONE);
-            }
-        }
-        /**
-         * Shows this question on the screen.
-         */
-        public void show() {
-            for (View view : currentQuestion.viewList) {
-                view.setVisibility(View.VISIBLE);
-            }
-            view_questionText.setText(currentQuestion.textId);
-
-            switch (this.type) {
-                case Single:
-                    for (int i = 0; i < this.viewList.size(); i++) {
-                        RadioButton view = (RadioButton) viewList.get(i);
-                        view.setText(this.answerList.get(i));
-                    }
-                    view_radioGroup.clearCheck();
-                    break;
-                case Multiple:
-                    for (int i = 0; i < this.viewList.size(); i++) {
-                        CheckBox view = (CheckBox) viewList.get(i);
-                        view.setText(this.answerList.get(i));
-                        view.setChecked(false);
-                    }
-                    break;
-                case Integer:
-                    //See: https://stackoverflow.com/questions/17805040/how-to-create-a-number-picker-dialog/17806895#17806895
-                    view_numberPicker.setMinValue(this.rangeMin);
-                    view_numberPicker.setMaxValue(this.rangeMax);
-                    //Use: https://stackoverflow.com/questions/12979643/change-the-step-size-of-a-numberpicker/30469826#30469826
-            }
-        }
-        /**
-         * Checks the user's answer.
-         * @return - Whether or not the user chose correctly
-         */
-        public boolean check() {
-            switch (this.type) {
-                case Single:
-                    for (int i = 0; i < this.viewList.size(); i++) {
-                        RadioButton view = (RadioButton) viewList.get(i);
-                        if (view.isChecked() != this.correctIndex.contains(i)) {
-                            return false;
-                        }
-                    }
-                    return true;
-                case Multiple:
-                    for (int i = 0; i < this.viewList.size(); i++) {
-                        CheckBox view = (CheckBox) viewList.get(i);
-                        if (view.isChecked() != this.correctIndex.contains(i)) {
-                            return false;
-                        }
-                    }
-                    return true;
-                case Integer:
-                    return view_numberPicker.getValue() == this.correctValue;
-            }
-            return false;
-        }
-        /**
-         * Returns a formatted message saying that the user chose correctly
-         * @return The message to show in the dialog box
-         * See: https://www.baeldung.com/java-random-list-element#highlighter_812240
-         */
-        public String getMessage_correct() {
-            return getResources().getString(this.correctId, correctList[random.nextInt(correctList.length)]);
-        }
-        /**
-         * Returns a formatted message saying that the user chose incorrectly
-         * @return The message to show in the dialog box
-         */
-        public String getMessage_incorrect() {
-            return getResources().getString(this.incorrectId, incorrectList[random.nextInt(incorrectList.length)]);
-        }
-    }
 
     /**
      * Fills the question array with structured question objects.
      */
     private void generateQuestions() {
         questions.clear();
-        questions.add(new Question(
+        questions.add(new Question(this,
+                R.drawable.word_tiles_fear,
                 R.string.quiz1_1,
                 R.string.quiz1_1_correct,
                 R.string.quiz1_1_incorrect,
                 new int[] {R.string.quiz1_1_a1, R.string.quiz1_1_a2},
                 0
         ));
-        questions.add(new Question(
+        questions.add(new Question(this,
+                R.drawable.dumbbell,
                 R.string.quiz1_2,
                 R.string.quiz1_2_correct,
                 R.string.quiz1_2_incorrect,
                 new int[] {R.string.quiz1_2_a1, R.string.quiz1_2_a2},
                 1
         ));
-        questions.add(new Question(
+        questions.add(new Question(this,
+                R.drawable.survival_kit,
                 R.string.quiz1_3,
                 R.string.quiz1_3_correct,
                 R.string.quiz1_3_incorrect,
                 new int[] {R.string.quiz1_3_a1, R.string.quiz1_3_a2},
                 1
         ));
-        questions.add(new Question(
+        questions.add(new Question(this,
+                R.drawable.pillars,
                 R.string.quiz2_1,
                 R.string.quiz2_1_correct,
                 R.string.quiz2_1_incorrect,
                 new int[] {R.string.quiz2_1_a1, R.string.quiz2_1_a2, R.string.quiz2_1_a3, R.string.quiz2_1_a4},
                 new int[] {0, 1, 2, 3}
         ));
-        questions.add(new Question(
+        questions.add(new Question(this,
+                R.drawable.water,
                 R.string.quiz3_1,
                 R.string.quiz3_1_correct,
                 R.string.quiz3_1_incorrect,
@@ -289,7 +123,8 @@ public class QuizActivity extends AppCompatActivity {
                 6,
                 2
         ));
-        questions.add(new Question(
+        questions.add(new Question(this,
+                R.drawable.food_storage,
                 R.string.quiz3_2,
                 R.string.quiz3_2_correct,
                 R.string.quiz3_2_incorrect,
@@ -297,7 +132,8 @@ public class QuizActivity extends AppCompatActivity {
                 12,
                 3
         ));
-        questions.add(new Question(
+        questions.add(new Question(this,
+                R.drawable.bug_out_bag,
                 R.string.quiz3_3,
                 R.string.quiz3_3_correct,
                 R.string.quiz3_3_incorrect,
